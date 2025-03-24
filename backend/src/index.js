a3cfe1178421e2e224406bd0dee35776394e130c
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2025-03-25 03:58:13 +0700

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,14 +33,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // ให้อนุญาต origin ที่ตรงกับใน allowedOrigins หรือถ้าไม่มีก็ให้อนุญาตทุก origin
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true, // Allow cookies and authorization headers
   })
 );
